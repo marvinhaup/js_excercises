b202304e65fd419e64411836d32f5da8a008f5ba
Marvin Haupenthal <marvinhaup@posteo.de>
2021-11-17 22:59:47 +0100

    finished chapter 5 excercise 1 (load file)

--- eigene Übungen/Übung 5 Asynchrone Programmierung/Aufgabe 1 Musikalben/albums.js
+++ eigene Übungen/Übung 5 Asynchrone Programmierung/Aufgabe 1 Musikalben/albums.js
@@ -1,21 +1,36 @@
-var loadButton = document.getElementById("btn");
+function loadFileAndInsert() {
+    let albumContent;
 
-loadButton.addEventListener("onclick", function() {
-    fetch('albums.json')
-    .then(response => response.json())
-    .then(data => console.log(data));
-})
-
-/*
-function loadAlbums() {
-    let fname = "Albums.json";
-    try {
-        let res = await fetch(fname);
-        return await res.json();
-    } catch (error) {
-        console.log(error);
-    }
+    fetch("albums.json")
+        .catch("Die datei konnte nicht gefunden werden")
+        .then(response => response.json())
+        .catch( () => alert("Die datei liegt nicht im json-Format vor.") )
+        .then(values => {
+            let container = document.getElementById("tableContainer");
+            container.appendChild(document.createElement("table"));
+            container.children[0].appendChild(document.createElement("thead"));
+            container.children[0].appendChild(document.createElement("tbody"));
+            let tableHead = document.querySelector("thead");
+            let tableBody = document.querySelector("tbody");
+            tableHead.appendChild(document.createElement("tr"));
+            for (let prop in values[0]) {
+                let elem = document.createElement("th");
+                elem.innerText = prop;
+                tableHead.children[0].appendChild(elem);
+            }
+            let tbrowCount = 0;
+            for (let obj of values) {
+                tableBody.appendChild(document.createElement("tr"));
+                for (let prop in obj) {
+                    let elem = document.createElement("td");
+                    elem.innerText = obj[prop];
+                    tableBody.children[tbrowCount].appendChild(elem);
+                }
+                tbrowCount++;
+            }
+        });
 }
 
-loadButton.addEventListener("onclick", );
-*/
+
+let loadBtn = document.getElementById("btn");
+loadBtn.addEventListener("click", loadFileAndInsert);
