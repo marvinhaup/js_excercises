27038bfd74f51c5f3db7c9ffec315bdf58b5fee0
Marvin Haupenthal <marvinhaup@posteo.de>
2021-12-16 11:11:30 +0100

    added genrecontroller

--- express-locallibrary-tutorial/controllers/genreController.js
+++ express-locallibrary-tutorial/controllers/genreController.js
@@ -4,8 +4,14 @@ var async = require('async');
 
 
 // Display list of all Genre.
-exports.genre_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre list');
+exports.genre_list = function(req, res, next) {
+    Genre.find()
+        .sort([['name', 'ascending']])
+        .exec(function (err, list_genres) {
+            if (err) { return next(err); }
+            // Successful, so render.
+            res.render('genre_list', { title: 'Genre List', genre_list:  list_genres});
+        });  
 };
 
 // Display detail page for a specific Genre.
@@ -34,6 +40,7 @@ exports.genre_detail = function(req, res, next) {
     });
 
 };
+
 
 
 // Display Genre create form on GET.
